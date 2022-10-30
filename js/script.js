db17e944c94bb7467e07698f101e145b4c2d2b5f
Christina Silitonga <91734751+ChristinaSilitonga@users.noreply.github.com>
2022-10-30 22:30:29 +0100

    data
    saya
    
    Signed-off-by: Christina Silitonga <91734751+ChristinaSilitonga@users.noreply.github.com>

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
